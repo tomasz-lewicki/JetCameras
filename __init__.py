d8ff3729c9bd9d6eb06e9339aecbdc856878b2cf
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-11-24 12:26:41 -0800

    changed gst string to f-string format

--- __init__.py
+++ __init__.py
@@ -1,49 +1,39 @@
 import cv2
 
-
 def gstreamer_pipeline(
     camera_id=0,
     capture_width=3264,
     capture_height=2464,
-    display_width=1024,
-    display_height=768,
+    out_width=1024,
+    out_height=768,
     framerate=10,
     flip_method=0,
 ):
 
-    return (
-        f"nvarguscamerasrc sensor_id=%d ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! "
-        "appsink max-buffers=1 drop=true"
-        % (
-            camera_id,
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
+    gst_string = f"""
+    nvarguscamerasrc sensor_id={camera_id} ! 
+    video/x-raw(memory:NVMM),
+    width=(int){capture_width}, height=(int){capture_height},
+    format=(string)NV12, framerate=(fraction){framerate}/1 !
+    nvvidconv flip-method={flip_method} !
+    video/x-raw, width=(int){out_width}, height=(int){out_height}, format=(string)BGRx !
+    videoconvert !
+    video/x-raw, format=(string)BGR !
+    appsink max-buffers=1 drop=true
+    """
+
+    return gst_string
+
 
 
 def make_imx219_capture(camera_id):
 
     gst_string = gstreamer_pipeline(
-        camera_id=0,
-        capture_width=3264,
-        capture_height=2464,
-        display_width=1024,
-        display_height=768,
+        camera_id=camera_id,
         framerate=21,
         flip_method=2,
     )
+
     cap = cv2.VideoCapture(gst_string, cv2.CAP_GSTREAMER)
 
     return cap
