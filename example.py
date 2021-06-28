703401f3244c627cc83d5ac35e2e4f42b0b9e831
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-06-28 16:20:39 -0700

    Add example.py

---
+++ example.py
@@ -0,0 +1,14 @@
+import time
+from JetCameras import IMX219
+
+
+if __name__ == "__main__":
+    
+    camera = IMX219(**IMX219.Configs.HD_60FPS)
+
+    while True:
+
+        img = camera.read()
+        print(img)
+        time.sleep(.1)
+
