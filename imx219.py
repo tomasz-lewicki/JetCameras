31bc57d33dfdf61688b8651dbfa77a817ebb47ba
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-07-20 16:18:18 -0700

    Move common logic from imx219.py to basecamera.py

--- imx219.py
+++ imx219.py
@@ -1,53 +1,12 @@
-import cv2
+from .basecamera import Camera
 
-class IMX219:
-    def __init__(
-        self,
-        camera_id=0,
-        capture_shape=(3264, 2464),
-        resize_to=(1024, 768),
-        fps=21,
-        flip=False,
-    ) -> None:
-
-        # Capture Part
-        source_str = f"""
-        nvarguscamerasrc sensor_id={camera_id} ! 
-        video/x-raw(memory:NVMM),
-        width=(int){capture_shape[0]}, height=(int){capture_shape[1]},
-        format=(string)NV12, framerate=(fraction){fps}/1 ! """
-
-        # Convertion Part
-        if resize_to is not None:
-            (out_width, out_height) = (resize_to[0], resize_to[1])
-        else:
-            (out_width, out_height) = (capture_shape[0], capture_shape[1])
-
-        conv_str = f"nvvidconv flip-method={2 if flip is True else 0} ! video/x-raw, width=(int){out_width}, height=(int){out_height}, format=(string)BGRx ! "
-
-        # Sink Part
-        sink_str = f"videoconvert ! video/x-raw, format=(string)BGR ! appsink max-buffers=1 drop=true"
-
-
-        gst_str = ""
-        gst_str += source_str
-        gst_str += conv_str if (resize_to or flip) else " "
-        gst_str += sink_str
-
-        self._gst_cap = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
-        print(gst_str)
-
-    def read(self):
-        retv, arr = self._gst_cap.read()
-        return arr
-
-    class Configs:
-
+class IMX219(Camera):
+    
+    class MODES:
         FULL_FRAME = {"capture_shape": (3264, 2464), "fps": 21}
-
         FHD29FPS = {"capture_shape": (1920, 1080), "fps": 29}
-
         HD_30FPS = {"capture_shape": (1280, 720), "fps": 30}
-
         HD_60FPS = {"capture_shape": (1280, 720), "fps": 60}
-        
+
+    def __init__(self):
+        super().__init__(**self.MODES.FULL_FRAME)
